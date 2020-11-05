640c5d61c7ae63d652d5ca3c7b095522a74868f5
gytautele <gytaute.barzdziute@cgi.com>
2020-11-05 15:50:08 +0200

    design

--- IsmanusisKurjeris/app/src/main/java/com/ismanusiskurjeris/ismanusiskurjeris/Main2Activity.kt
+++ IsmanusisKurjeris/app/src/main/java/com/ismanusiskurjeris/ismanusiskurjeris/Main2Activity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -14,6 +16,12 @@ class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+            fun onCreateOptionsMenu(menu: Menu):Boolean {
+                val inflater = getMenuInflater()
+                inflater.inflate(R.menu.my_options_menu, menu)
+                return true
+            }
 
         //Send: intent.putExtra("Username", username.text.toString())
         //Receive: var strUser: String = intent.getStringExtra("Username")
