253e0d074fae4a9d04ee7bded7fe9929fd306d3a
Tomura69 <tomas.yee@gmail.com>
2020-11-11 10:29:05 +0200

    Menu buttons

--- IsmanusisKurjeris/app/src/main/java/com/ismanusiskurjeris/ismanusiskurjeris/Main2Activity.kt
+++ IsmanusisKurjeris/app/src/main/java/com/ismanusiskurjeris/ismanusiskurjeris/Main2Activity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
@@ -28,15 +29,39 @@ class Main2Activity : AppCompatActivity() {
         //Toast.makeText(applicationContext, "Puslapis veikia", Toast.LENGTH_SHORT).show()
 
         val searchButton = findViewById<Button>(R.id.searchButton)
+        val searchButton2 = findViewById<Button>(R.id.searchButton2)
         searchButton.setOnClickListener {
-
             val packageId = findViewById<EditText>(R.id.packageId) as EditText
             //Siuntimui i duombaze packageId.text.toString()
-
             val intent = Intent(this, Main3Activity::class.java)
             startActivity(intent)
         }
 
+        val ieskoti = findViewById<Button>(R.id.ieskoti)
+        val siuntos = findViewById<Button>(R.id.siuntos)
+        val nustatymai = findViewById<Button>(R.id.nustatymai)
+        val atsijungti = findViewById<Button>(R.id.atsijungti)
+        val paskyra = findViewById<Button>(R.id.paskyra)
 
+        ieskoti.setOnClickListener {
+            val intent = Intent(this, Main2Activity::class.java)
+            startActivity(intent)
+        }
+        siuntos.setOnClickListener {
+            val intent = Intent(this, Main3Activity::class.java)
+            startActivity(intent)
+        }
+        nustatymai.setOnClickListener {
+            val intent = Intent(this, Main3Activity::class.java)
+            startActivity(intent)
+        }
+        atsijungti.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        paskyra.setOnClickListener {
+            val intent = Intent(this, Main4Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- IsmanusisKurjeris/app/src/main/java/com/ismanusiskurjeris/ismanusiskurjeris/Main3Activity.kt
+++ IsmanusisKurjeris/app/src/main/java/com/ismanusiskurjeris/ismanusiskurjeris/Main3Activity.kt
@@ -24,5 +24,32 @@ class Main3Activity : AppCompatActivity() {
             val intent = Intent(this, Main4Activity::class.java)
             startActivity(intent)
         }
+
+        val ieskoti = findViewById<Button>(R.id.ieskoti)
+        val siuntos = findViewById<Button>(R.id.siuntos)
+        val nustatymai = findViewById<Button>(R.id.nustatymai)
+        val atsijungti = findViewById<Button>(R.id.atsijungti)
+        val paskyra = findViewById<Button>(R.id.paskyra)
+
+        ieskoti.setOnClickListener {
+            val intent = Intent(this, Main2Activity::class.java)
+            startActivity(intent)
+        }
+        siuntos.setOnClickListener {
+            val intent = Intent(this, Main3Activity::class.java)
+            startActivity(intent)
+        }
+        nustatymai.setOnClickListener {
+            val intent = Intent(this, Main3Activity::class.java)
+            startActivity(intent)
+        }
+        atsijungti.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        paskyra.setOnClickListener {
+            val intent = Intent(this, Main4Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
