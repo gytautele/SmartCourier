e8b3f1ea061b723543d1c2aecf22c247f137a62b
Tomura69 <tomas.yee@gmail.com>
2020-12-04 22:51:18 +0200

    Image button neveikia, todel jo nenaudok, ideta duombaze su scriptu, ji veikia

--- IsmanusisKurjeris/app/src/main/java/com/ismanusiskurjeris/ismanusiskurjeris/MainActivity.kt
+++ IsmanusisKurjeris/app/src/main/java/com/ismanusiskurjeris/ismanusiskurjeris/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.ismanusiskurjeris.ismanusiskurjeris
 
 import android.content.Intent
-import android.database.sqlite.SQLiteDatabase
-import android.support.v7.app.AppCompatActivity
+import android.database.Cursor
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -23,7 +24,29 @@ class MainActivity : AppCompatActivity() {
             //Siuntimui i duombaze password.text.toString()
 
             val db = dbManager(this)
-            Toast.makeText(this, db.addUser(username.text.toString(), password.text.toString()), Toast.LENGTH_LONG).show()
+            val checkinsertdata: Boolean = db.insertuserdata(username.text.toString(), password.text.toString(), "LABAS")
+            if (checkinsertdata == true) Toast.makeText(this@MainActivity, "New Entry Inserted", Toast.LENGTH_SHORT).show()
+            else Toast.makeText(this@MainActivity, "New Entry Not Inserted", Toast.LENGTH_SHORT).show()
+
+            val res = db.getdata();
+            if(res.getCount()==0){
+                Toast.makeText(this, "No Entry Exists", Toast.LENGTH_SHORT).show();
+            }
+            val buffer = StringBuffer();
+            while(res.moveToNext()){
+                buffer.append("Name :"+res.getString(0)+"\n");
+                buffer.append("Contact :"+res.getString(1)+"\n");
+                buffer.append("Date of Birth :"+res.getString(2)+"\n\n");
+            }
+
+            val builder = AlertDialog.Builder(this);
+            builder.setCancelable(true);
+            builder.setTitle("User Entries");
+            builder.setMessage(buffer.toString());
+            builder.show()
+
+
+            //Toast.makeText(this, db.addUser(username.text.toString(), password.text.toString()), Toast.LENGTH_LONG).show()
 
             val intent = Intent(this, Main2Activity::class.java)
             startActivity(intent)
--- IsmanusisKurjeris/app/src/main/java/com/ismanusiskurjeris/ismanusiskurjeris/dbManager.java
+++ IsmanusisKurjeris/app/src/main/java/com/ismanusiskurjeris/ismanusiskurjeris/dbManager.java
@@ -2,42 +2,81 @@ package com.ismanusiskurjeris.ismanusiskurjeris;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
 
 public class dbManager extends SQLiteOpenHelper {
-
-    private static final String dbName = "SmartKurjer.db";
-
     public dbManager(Context context) {
-        super(context, dbName, null, 1);
+        super(context, "Userdata.db", null, 1);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        String qry = "CREATE TABLE user (id integer PRIMARY KEY AUTOINCREMENT, name text, password text)";
-        db.execSQL(qry);
+    public void onCreate(SQLiteDatabase DB) {
+        //DB.execSQL("create Table Userdetails(name TEXT primary key, contact TEXT, dob TEXT)");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS user");
-        onCreate(db);
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        DB.execSQL("drop Table if exists Userdetails");
     }
 
-    public String addUser(String p1, String p2) {
-        SQLiteDatabase db = this.getWritableDatabase();
+    public Boolean insertuserdata(String name, String contact, String dob)
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("contact", contact);
+        contentValues.put("dob", dob);
+        long result=DB.insert("Userdetails", null, contentValues);
+        if(result==-1){
+            return false;
+        }else{
+            return true;
+        }
+    }
 
-        ContentValues cv = new ContentValues();
-        cv.put("name", p1);
-        cv.put("password", p2);
 
-        long res = db.insert("user", null, cv);
+    public Boolean updateuserdata(String name, String contact, String dob) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("contact", contact);
+        contentValues.put("dob", dob);
+        Cursor cursor = DB.rawQuery("Select * from Userdetails where name = ?", new String[]{name});
+        if (cursor.getCount() > 0) {
+            long result = DB.update("Userdetails", contentValues, "name=?", new String[]{name});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }}
 
-        if (res == -1)
-            return "Failed";
-        else
-            return "Succesfully inserted";
+
+    public Boolean deletedata (String name)
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from Userdetails where name = ?", new String[]{name});
+        if (cursor.getCount() > 0) {
+            long result = DB.delete("Userdetails", "name=?", new String[]{name});
+            if (result == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+
+    }
+
+    public Cursor getdata ()
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from Userdetails", null);
+        return cursor;
+
     }
 }
